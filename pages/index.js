e32299cccc54571efecf9dd62289cae2b79fad30
Alfredo R. Pabon <alfredrafael@gmail.com>
2025-01-30 08:21:33 -0600

    Add tailwind to repo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -10,14 +10,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="">
         <Header title="Welcome to my app!" />
-        <p className="description">
+        <p className="description text-3xl font-bold underline">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
